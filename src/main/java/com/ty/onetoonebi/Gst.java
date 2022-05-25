3dd84657a76c8a1a85b47f227fa13d03d04b0e38
Karthikeyan180400 <karthikeyan180400@gmail.com>
2022-05-25 16:15:12 +0530

    fetch_type_applied

--- src/main/java/com/ty/onetoonebi/Gst.java
+++ src/main/java/com/ty/onetoonebi/Gst.java
@@ -1,6 +1,7 @@
 package com.ty.onetoonebi;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,7 +16,7 @@ public class Gst {
 	String gstNumber;
 	String state;
 	String country;
-	@OneToOne
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "my_comp_id")
 	Company company;
 
